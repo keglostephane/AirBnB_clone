8e61657c1c70e28e0641835933ad2c44747e93d7
Chesterkxng <cgoita00@gmail.com>
2023-12-06 13:50:00 +0000

    useless blank line

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,6 @@ class BaseModel:
                         setattr(self, attr, datetime.fromisoformat(value))
                     else:
                         setattr(self, attr, value)
-
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
