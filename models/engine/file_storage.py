6a386abbb3d065fb811e01ddfa8a992c8f32cf65
Chesterkxng <cgoita00@gmail.com>
2023-12-07 17:23:57 +0000

    update to manange all new classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,11 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
@@ -44,5 +49,15 @@ class FileStorage:
                         self.__objects[key] = BaseModel(**value)
                     elif cls_name == "User":
                         self.__objects[key] = User(**value)
+                    elif cls_name == "State":
+                        self.__objects[key] = State(**value)
+                    elif cls_name == "City":
+                        self.__objects[key] = City(**value)
+                    elif cls_name == "Amenity":
+                        self.__objects[key] = Amenity(**value)
+                    elif cls_name == "Place":
+                        self.__objects[key] = Place(**value)
+                    elif cls_name == "Review":
+                        self.__objects[key] = Review(**value)
         except FileNotFoundError:
             pass
