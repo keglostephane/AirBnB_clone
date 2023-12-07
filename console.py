554a56b3d0e1a3e02cd624730041d0167956bb8a
Dodzi Keglo <stephanekeglo@gmail.com>
2023-12-07 14:15:08 +0000

    Create the console for objects manipulation

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Console module
+"""
+import cmd
+import sys
+
+
+class HBNBCommand(cmd.Cmd):
+    """AirBnB clone console"""
+    prompt = '(hbnb)'
+
+    def emptyline(self):
+        """command to execute when an empty line is provided"""
+        pass
+
+    def do_EOF(self, line):
+        """EOF command to exit the program
+        """
+        return True
+
+    def do_quit(self, line):
+        """Quit command to exit the program
+        """
+        sys.exit()
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
