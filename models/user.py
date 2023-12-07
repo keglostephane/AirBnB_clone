806c56a4e61a3a368fb134a82de54435c890840c
Chesterkxng <cgoita00@gmail.com>
2023-12-07 15:55:30 +0000

    User class

---
+++ models/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" User module
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Define all common attributes and methods for child classes.
+
+    :param email: user's email
+    :type email: str
+    :param password: user's password
+    :type password: str
+    :param first_name: user's first name
+    :type first_name: str
+    :param last_name: user's last name
+    :type last_name: str
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
