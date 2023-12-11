916c64a1f19a45d66a1642ab43d27fa4d978b38f
Dodzi Keglo <stephanekeglo@gmail.com>
2023-12-11 01:16:50 +0000

    Add tests for State class

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Unittest for State class
+"""
+import unittest
+from models.state import State
+
+
+class testState(unittest.TestCase):
+    """Test the State class"""
+
+    def setUp(self):
+        """Create State instance for tests"""
+        self.state = State()
+
+    def tearDown(self):
+        """Destroy State instance after tests"""
+
+    def testStateClassPublicAttributes(self):
+        """Test if State class have public attributes"""
+        self.assertNotIn('name', self.state.__dict__)
+        self.assertIn('name', State.__dict__)
+        self.assertEqual(State.name, '')
+        self.assertIs(type(State.name), str)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,9 +2,7 @@
 """Unittest for User class
 """
 import unittest
-import json
 from models.user import User
-from models import storage
 
 
 class testUser(unittest.TestCase):
@@ -23,3 +21,18 @@ class testUser(unittest.TestCase):
         self.assertNotIn('password', self.user.__dict__)
         self.assertNotIn('last_name', self.user.__dict__)
         self.assertNotIn('first_name', self.user.__dict__)
+
+        self.assertIn('email', User.__dict__)
+        self.assertIn('password', User.__dict__)
+        self.assertIn('last_name', User.__dict__)
+        self.assertIn('first_name', User.__dict__)
+
+        self.assertEqual(User.email, '')
+        self.assertEqual(User.password, '')
+        self.assertEqual(User.last_name, '')
+        self.assertEqual(User.first_name, '')
+
+        self.assertIs(type(User.email), str)
+        self.assertIs(type(User.password), str)
+        self.assertIs(type(User.last_name), str)
+        self.assertIs(type(User.first_name), str)
