cc998d512a8562f7149fa100b6c370cf0d07bfd4
Dodzi Keglo <stephanekeglo@gmail.com>
2023-12-11 01:34:54 +0000

    Add tests for Amenity class

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,23 @@
+#!/usr/bin/bin/python3
+"""Unittest for User class
+"""
+import unittest
+from models.amenity import Amenity
+
+
+class testAmenity(unittest.TestCase):
+    """Test the Amenity class"""
+
+    def setUp(self):
+        """Create Amenity instance for tests"""
+        self.amenity = Amenity()
+
+    def tearDown(self):
+        """Destroy Amenity instance after tests"""
+
+    def testAmenityClassPublicAttribute(self):
+        """Test if Amenity class have public attributes"""
+        self.assertNotIn('name', self.amenity.__dict__)
+        self.assertIn('name', Amenity.__dict__)
+        self.assertEqual(Amenity.name, '')
+        self.assertIs(type(Amenity.name), str)
